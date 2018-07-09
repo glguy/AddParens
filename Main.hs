a5437097911065ba8fb6a64792744b38c0505d76
Eric Mertens <emertens@gmail.com>
2018-07-08 20:12:11 -0700

    Don't try to parenthesize type operators, HSE doesn't parse them correctly

--- Main.hs
+++ Main.hs
@@ -8,12 +8,12 @@ Maintainer  : emertens@gmail.com
 -}
 
 -- syb
-import Data.Generics (Data, everywhere, extT, mkT)
+import Data.Generics (Data, everywhere, ext1T)
 -- haskell-src-exts
 import Language.Haskell.Exts (Exp(..), Type(..), Mode(OneLineMode), PPLayout(PPNoLayout),
                               Style(mode), PPHsMode(layout), ParseResult(..), Module,
                               Pretty, SrcSpanInfo(..), Parseable(parseWithMode),
-                              ParseMode(..), glasgowExts,
+                              ParseMode(..), glasgowExts, ann,
                               defaultMode, defaultParseMode, prettyPrintStyleMode, style)
 -- base
 import System.Environment (getArgs)
@@ -26,23 +26,26 @@ import Data.Proxy (Proxy(Proxy))
 -- | Ensure that infix and prefix applications are surrounded by parentheses
 -- and remove redundant parentheses. This rule only operates on the top-level
 -- of a particular expression.
-addParensExp1 :: Exp () -> Exp ()
-addParensExp1 x@InfixApp{}        = Paren () x -- add parentheses to infix application
-addParensExp1 x@App{}             = Paren () x -- add parentheses to prefix application
-addParensExp1 (Paren _ x@Paren{}) = x          -- remove nested parentheses
-addParensExp1 x                   = x          -- otherwise no transformation
+addParensExp1 :: Exp a -> Exp a
+addParensExp1 x@InfixApp{}        = Paren (ann x) x -- add parentheses to infix application
+addParensExp1 x@App{}             = Paren (ann x) x -- add parentheses to prefix application
+addParensExp1 (Paren _ x@Paren{}) = x               -- remove nested parentheses
+addParensExp1 x                   = x               -- otherwise no transformation
 
 -- | Apply same logic from 'addParensExp1' to types found in the expression.
-addParensType1 :: Type () -> Type ()
-addParensType1 x@TyInfix{}             = TyParen () x
-addParensType1 x@TyFun{}               = TyParen () x
-addParensType1 x@TyApp{}               = TyParen () x
+--
+-- Fixities aren't correctly supported for type operators, so this code doesn't
+-- bother parenthesizing them.
+-- <https://github.com/haskell-suite/haskell-src-exts/issues/47>
+addParensType1 :: Type a -> Type a
+addParensType1 x@TyFun{}               = TyParen (ann x) x
+addParensType1 x@TyApp{}               = TyParen (ann x) x
 addParensType1 (TyParen _ x@TyParen{}) = x
 addParensType1 x                       = x
 
 -- | Recursively parenthesize the expressions and types in a given value.
 addParens :: Data d => d -> d
-addParens = everywhere (mkT addParensExp1 `extT` addParensType1)
+addParens = everywhere (id `ext1T` addParensExp1 `ext1T` addParensType1)
             -- bottom up rewrite using syb
 
 ------------------------------------------------------------------------
@@ -52,19 +55,17 @@ addParens = everywhere (mkT addParensExp1 `extT` addParensType1)
 -- | Transform a Haskell expression to have parentheses making precedence
 -- more obvious.
 explicitParens ::
-  Functor f                 => -- functor used to remove source locations
-  Parseable (f SrcSpanInfo) => -- parsable with source locations
-  Pretty    (f ())          => -- printable without source locations
-  Data      (f ())          => -- supports generic traversals
+  Parseable (f SrcSpanInfo) =>
+  Pretty    (f SrcSpanInfo) =>
+  Data      (f SrcSpanInfo) =>
   proxy f {- ^ proxy for specifying syntax elements to process -} ->
   String  {- ^ parser input string                             -} ->
   String  {- ^ rendered, parenthesized output                  -}
-explicitParens pxy = render . fmap (addParens . forgetAnn pxy) . parse
-
--- | Forget the source location annotations. The extra proxy argument
--- is to help type inference along in the implementation of 'explicitParens'.
-forgetAnn :: Functor f => proxy f -> f SrcSpanInfo -> f ()
-forgetAnn _proxy x = () <$ x
+explicitParens pxy = render . fmap (addParens . fixType pxy) . parse
+  where
+    -- Link parsed type to the proxy argument and specify SrcSpanInfo
+    fixType :: proxy f -> f SrcSpanInfo -> f SrcSpanInfo
+    fixType _ x = x
 
 -- | Pretty print a syntax element on a single line using explicit layout.
 flatPrettyPrint :: Pretty a => a -> String
